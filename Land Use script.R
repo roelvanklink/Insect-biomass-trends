7d582eb69c72dc858881f417738173426659db59
Roel van Klink <roel.vanklink@gmail.com>
2019-04-11 14:33:17 +0200

    Added script for prepping ESA CCI data for analyses: percentage cover of urban and crop for 9 cells

--- Land Use script.R
+++ Land Use script.R
@@ -2,17 +2,24 @@
 
 setwd("C:\\Dropbox\\Dropbox\\Insect Biomass Trends/csvs") # work
 
+load("metadata_per_plot.RData")
+
+
 # land-use harmonization databse: 
-LU<-read.table("landuseData2.txt", header = T)
+LU<-read.table("landuseData (2).txt", header = T)
 dim(LU)
 head(LU)
 
+
+
+
 # ESA Land Cover State products
-LU2<-read.table("LU_CCIESA_1992on.txt", header = T)
-LU2nbrs<- read.table("LU_CCIESA_neighbours_1992on.txt", header = T)
+LU2<-read.table("LU_CCIESA_1992on (2).txt", header = T)
+LU2nbrs<- read.table("LU_CCIESA_neighbours_1992on (2).txt", header = T)
 hist(LU2$X2015, breaks=seq(5,225,by=1)) # distribution of sites over different categories 
 # were there any changes in landcover in this period? 
 changed<- (LU2[(LU2$X1992 - LU2$X2015 != 0)   , c(1,24:26) ]) # 67 sites  have a  change = 4%
+nrow(changed) / nrow (metadata_per_plot) # 4.2%
 # which changes do we have 
 unique(changed[,1:2])
 
@@ -22,6 +29,134 @@ unique(changed[,1:2])
 
 
 
+# LUH2
+
+#how much changed?
+(sum(LU$End_forestArea) - sum(LU$Start_forestArea)) / sum(LU$Start_forestArea) # forest: +0.039%
+(sum(LU$End_cropArea) - sum(LU$Start_cropArea)) / sum(LU$Start_cropArea) # crop: -0.09%
+(sum(LU$End_pastureArea) - sum(LU$Start_pastureArea)) / sum(LU$Start_pastureArea) # pasture: -0.045
+(sum(LU$End_urbanArea) - sum(LU$Start_urbanArea)) / sum(LU$Start_urbanArea) # urban: +0.19%
+
+# that's not unsubstantial, potentially interesting
+
+# how many locations experienced urbanization over the sampling period?
+urbanization<- (LU$End_urbanArea - LU$Start_urbanArea) 
+hist(sqrt(unique(urbanization)), xlab = "urbanization")
+
+cropification<- LU$End_cropArea - LU$Start_cropArea
+hist(log(cropification))
+
+
+
+
+
+
+
+######################################################################
+
+#Prep ESA data for analysis (% of each LU type in final year)
+
+#1) combine the file for main cell and neighbor cells
+
+LU2nbrs<- LU2nbrs[, c(25,1:24)]
+LU2IDs<-LU2[, c(1,26,27)]
+LU2<- LU2[, 1:25]
+LU2<- unique(LU2) # keep only 1 value for each mainCell
+
+LU2<- rbind(LU2, LU2nbrs)
+dim(LU2)/9  # 1023 unique cells
+dim(LU2nbrs)
+
+fullLU2<-merge(LU2, LU2IDs) # 
+dim(fullLU2)/9  #= 1566 plots! correct 
+
+changed<- (fullLU2[(fullLU2$X1992 - fullLU2$X2015 != 0)   , c(1,24:26) ])
+nrow(changed)/ nrow(fullLU2)
+# make matrix 
+
+library(reshape2)
+#TRy out for 2015 values
+counts<- dcast(LU2, mainCell~X2015, length)
+counts$frcCrop<- (counts$`10` / 9) + (counts$`11`/9)  + (counts$`12`/9) + (counts$`30`*0.75)/9 + (counts$`40`*0.25)/9  
+counts$frcUrban<-counts$`190` / 9
+hist((counts$frcCrop))
+# works
+
+
+
+valueLastYr<-NULL
+
+
+for(i in 1: nrow(fullLU2)){
+plt<- fullLU2$Plot_ID[i]
+
+year<- metadata_per_plot$End_year[metadata_per_plot$Plot_ID == plt]
+true.yr<-year
+if (length(year) == 0) {year <-0} # if plotID is missing, replce year  by 0
+if(year>2015){  year<-2015} # for yrs after 2015 use 2015
+if (year<1992  & year >1986){year <- 1992} # for yrs between 1987 and 1991 use 1992
+
+yearx<- paste0( "X", year)
+value <- fullLU2[i, yearx]
+
+if(true.yr < 1987){value <-NA} # for yrs before 1987 use NA
+
+valueLastYr[i]<- value
+}
+
+# calculate % cover for each plot
+fullLU2$valueLastYr<-valueLastYr
+
+counts<- dcast(na.omit(fullLU2), Plot_ID~valueLastYr, length)
+dim(counts)  # 40 plots lost because too old
+counts$frcCrop<- (counts$`10` / 9) + (counts$`11`/9)  + (counts$`12`/9) + (counts$`30`*0.75)/9 + (counts$`40`*0.25)/9  
+counts$frcUrban<-counts$`190` / 9
+hist((counts$frcCrop))
+hist(counts$frcUrban)
+
+
+percCover900m <-counts 
+save(percCover900m, file = "percCover900m.RData")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# junk
 
 
 LU$EndTotalLandCover<- rowSums(LU[, c(13,15,17,19)])
@@ -92,64 +227,6 @@ grid.arrange(p1,p2,p3,p4, nrow = 2)
 
 ggplot(test, aes(x= End_cropArea, y= slope, color = Realm)) + 
   geom_point()  +     
-facet_wrap(~Continent)
+  facet_wrap(~Continent)
 
 
-
-######################################################################
-
-#Prep ESA data for analysis (% of each LU type in final year)
-
-#1) combine the file for main cell and neighbor cells
-
-LU2nbrs<- LU2nbrs[, c(25,1:24)]
-LU2IDs<-LU2[, c(1,26,27)]
-LU2<- LU2[, 1:25]
-LU2<- unique(LU2) # keep only 1 value for each mainCell
-
-LU2<- rbind(LU2, LU2nbrs)
-dim(LU2)/9  # 1023 unique cells
-dim(LU2nbrs)
-
-fullLU2<-merge(LU2, LU2IDs) # doesnt work because of duplicate values in both tables
-dim(fullLU2)/9  #= 1661 plots! correct 
-# make matrix 
-
-#TRy out for 2015 values
-counts<- dcast(LU2, mainCell~X2015, length)
-counts$frcCrop<- (counts$`10` / 9) + (counts$`11`/9)  + (counts$`12`/9) + (counts$`30`*0.75)/9 + (counts$`40`*0.25)/9  
-counts$frcUrban<-counts$`190` / 9
-hist((counts$frcCrop))
-# works
-
-
-
-valueLastYr<-NULL
-
-
-for(i in 1: nrow(fullLU2)){
-plt<- fullLU2$Plot_ID[i]
-
-year<- metadata_per_plot$End_year[metadata_per_plot$Plot_ID == plt]
-
-if (length(year) == 0){year <-0} # if plotID is missing, replce year  by 0
-if(year>2015){  year<-2015} # for yrs after 2015 use 2015
-if (year<1992  & year >1986){year <- 1992} # for yrs between 1987 and 1991 use 1992
-
-yearx<- paste0( "X", year)
-value <- fullLU2[i, yearx]
-
-if(year < 1987){value <-NA} # for yrs before 1987 use NA
-
-valueLastYr[i]<- value
-}
-
-# calculate % cover for each plot
-fullLU2$valueLastYr<-valueLastYr
-
-counts<- dcast(fullLU2, Plot_ID~valueLastYr, length)
-counts$frcCrop<- (counts$`10` / 9) + (counts$`11`/9)  + (counts$`12`/9) + (counts$`30`*0.75)/9 + (counts$`40`*0.25)/9  
-counts$frcUrban<-counts$`190` / 9
-hist((counts$frcCrop))
-
-# 250 plots have no values
